2431a4680d4ef7a7897f8789aeefb4f61d730002
Uxbel <beldetes@gmail.com>
2021-09-16 06:47:15 +0000

    Database model

--- src/api/models.py
+++ src/api/models.py
@@ -1,19 +1,54 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship
 
 db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=False, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-
+    city_id = db.Column(db.Integer, db.ForeignKey('city.id'))
+    
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.name
 
     def serialize(self):
         return {
             "id": self.id,
+            "name": self.name,
             "email": self.email,
+            # do not serialize the password, its a security breach
+        }
+
+class Country(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    country_name = db.Column(db.String(120), unique=True, nullable=False)
+    cities = db.relationship('City', backref='country', lazy=True)
+
+    def __repr__(self):
+        return '<Country %r>' % self.country_name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "country_name": self.country_name,
+            # do not serialize the password, its a security breach
+        }
+
+class City(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    city = db.Column(db.String(120), unique=True, nullable=False)
+    country_id = db.Column(db.Integer, db.ForeignKey('country.id'), nullable=False)
+    users = db.relationship('User', backref='city', lazy=True)
+
+
+    def __repr__(self):
+        return '<City %r>' % self.city
+
+    def serialize(self):
+        return {
+            "id": self.city,
+            "city": self.city,
             # do not serialize the password, its a security breach
         }
--- src/api/routes.py
+++ src/api/routes.py
@@ -8,8 +8,54 @@ from api.utils import generate_sitemap, APIException
 api = Blueprint('api', __name__)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+@api.route('/', methods=['POST', 'GET'])
+def handle_home():
+
+    response_body = {
+        "message": "Hello! I'm a message that came from the backend"
+    }
+
+    return jsonify(response_body), 200
+
+@api.route('/login', methods=['POST'])
+def login():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    user = User.query.filter_by(email = User.email).first()
+    if(user is None):
+        return "user not exist", 404
+    #generar token y retornar el "access token"
+
+    response_body = {
+        "m": "Hello! I'm a message that came from the backend"
+    }
+
+    return jsonify(response_body), 200
+
+@api.route('/register', methods=['POST'])
+def register():
+    name = request.json.get("name", None)
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    city_id = request.json.get("city_id", None)
+    user = User.query.filter_by(email = User.email).first()
+    if(user):
+        return "user exist", 400
+    new_user = User(name = name, email = email, password = password, city_id = city_id)
+    db.session.add(new_user)
+    db.session.commit()
+    #generar token y retornar el "access token"
+        return "user created successfully", 200
+
+    response_body = {
+        "message": "Hello! I'm a message that came from the backend"
+    }
+
+    return jsonify(response_body), 200
+
+@api.route('/user', methods=['GET'])
+def user_info():
+    user = User.query.filter_by(id = User.id).first()
 
     response_body = {
         "message": "Hello! I'm a message that came from the backend"
